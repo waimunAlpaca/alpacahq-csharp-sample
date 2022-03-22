221be4f800074d5aba571e6f850a4034195456f9
Wai Mun <waimun.ng@alpaca.markets>
2022-03-22 17:31:43 +0800

    Post a MarketOrder.Buy order

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Alpaca.Markets;
+using System.Threading.Tasks;
+
+namespace AlpacaExample
+{
+    internal static class Program
+    {
+        private const String KEY_ID = "";
+        private const String SECRET_KEY = "";
+        private const String SYMBOL = "AAPL";
+        private const Decimal FractionalQuantity = 0.555M;
+
+        public static async Task Main()
+        {
+            var client = Environments.Paper
+                .GetAlpacaTradingClient(new SecretKey(KEY_ID, SECRET_KEY));
+
+            var clock = await client.GetClockAsync();
+
+            if (clock != null)
+            {
+                Console.WriteLine(
+                    "Timestamp: {0}, NextOpen: {1}, NextClose: {2}",
+                    clock.TimestampUtc, clock.NextOpenUtc, clock.NextCloseUtc);
+
+                var order = await client.PostOrderAsync(
+                    MarketOrder.Buy(SYMBOL, OrderQuantity.Fractional(FractionalQuantity)));
+
+                Console.WriteLine("Buy: " + SYMBOL + " OrderQuantity: " + FractionalQuantity);
+            }
+        }
+    }
+}
